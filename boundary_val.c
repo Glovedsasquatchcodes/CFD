b98aec03d8256dc0cb61f975d9a0740a34698da6
Sajal92 <38528450+Sajal92@users.noreply.github.com>
2018-04-23 00:08:06 +0200

    Update boundary_val.c

--- boundary_val.c
+++ boundary_val.c
@@ -4,10 +4,7 @@ void boundaryvalues(
   int imax,
   int jmax,
   double **U,
-  double **V,
-  double **P,
-  double **F,
-  double **G	
+  double **V	
 )
 {
 	for (int j = 1; j <= jmax; j++)
@@ -18,13 +15,6 @@ void boundaryvalues(
 		V[0][j] = -V[1][j];
 		V[imax+1][j] = -V[imax][j];
 		
-		P[0][j] = P[1][j];
-		P[imax+1][j] = P[imax][j];
-		
-		F[0][j] = U[0][j];
-		F[imax][j] = U[imax][j];
-		
-		
 	}
 	
 	for (int i = 1; i <= imax; i++)
@@ -34,12 +24,7 @@ void boundaryvalues(
 		
 		U[i][0] = -U[i][1];
 		U[i][jmax+1] = -U[i][jmax];
-		
-		P[i][0] = P[i][1];
-		P[i][jmax+1] = P[i][jmax];
-		
-		G[i][0] = V[i][0];
-		G[i][jmax] = V[i][jmax];
+	
 	}
 
 
